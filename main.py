fedc5a1abb1379800aea65d74af0591b04ab57a7
Alex Goodenbour <alg107@uclive.ac.nz>
2020-10-06 17:32:02 +1300

    vacc

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from scipy.interpolate import UnivariateSpline
 
 plt.style.use('Solarize_Light2')
 from scipy.stats import norm
+from scipy import signal
 
 
 
 default_params = {
         "gamma": 0.1,
+        "nu": (1/(30*4)),
         "InfDays": 10,
         "R0": [0.5, 1.0, 2.0, 3.0, 4.0],
-        "intervals": [0,50,120,200],
-        "y0": [1.0,0.0,0.0],
+        "intervals": [0,50,120,200, 300, 400, 450, 500],
+        "y0": [1.0,0.0,0.0,0.0],
         "import_packet": 6e-7 # 3 of 5,000,000
         }
 
@@ -29,21 +31,26 @@ def sir(y,t,beta, gamma, spl):
             ]
     return dydt
 
-def sirv(y,t,beta, gamma):
-    # Unfinished
-    S,I,R=y
+def sirv(y,t,beta, gamma, nu, spl):
+    S,I,R,V=y
+    #betan = betan*bm_samples[int(np.floor(tn*10))]
+    vaccf = lambda x: 0.5*(signal.square(x/(2*np.pi)/3)+1)*0.002
+    
     dydt = [
-            -beta*I*S-b*S+b*(1-m)*(S+R),
-            beta*I*S - (pd+r)*I,
-            r*I-b*R+mb*(S+R)
+            #(np.sin(t)*0.1+1)
+            -beta*spl(t)*I*S -vaccf(t)*S + nu*V,
+            beta*spl(t)*I*S - spl(t)*gamma*I,
+            spl(t)*gamma*I,
+            vaccf(t)*S-nu*V 
             ]
     return dydt
 
+
 def runsir(params, ti, tf, fn):
-    gamma, R0, InfDays, y0 = params
+    gamma, nu, R0, InfDays, y0 = params
     beta = R0/InfDays
     t = np.linspace(ti,tf,1000)
-    sol = odeint(fn, y0, t, args=(beta, gamma, spl))
+    sol = odeint(fn, y0, t, args=(beta, gamma, nu, spl))
     return t, sol
 
 def gen_intervals(tf):
@@ -73,13 +80,14 @@ def runsim(p, fn):
     intervals = p["intervals"]
     y0 = p["y0"]
     import_packet = p["import_packet"]
+    nu = p["nu"]
     
     for R in R0:
         final_t = np.array([])
         final_sol = np.array([])
         y0_int = np.copy(y0)
         for i, interval in enumerate(intervals[:-1]):
-            params = [gamma, R, InfDays, y0_int]
+            params = [gamma, nu, R, InfDays, y0_int]
             t, sol = runsir(params,intervals[i], intervals[i+1], fn)
             final_t = np.concatenate((final_t, t)) if final_t.size else t
             final_sol = np.concatenate((final_sol, sol)) if final_sol.size else sol
@@ -90,12 +98,17 @@ def runsim(p, fn):
         plt.plot(final_t, final_sol[:,0])
         plt.plot(final_t, final_sol[:,1])
         plt.plot(final_t, final_sol[:,2])
-        plt.legend(["S","I","R"])
+        plt.plot(final_t, final_sol[:,3])
+        plt.legend(["S","I","R","V"])
 
-x, spl = gen_noisefn(200) # Arg must be as long as needed
+x, spl = gen_noisefn(500) # Arg must be as long as needed
 plt.plot(x, spl(x))
 
-runsim(default_params, sir)
+runsim(default_params, sirv)
+vaccf = lambda x: 0.5*(signal.square(x/(2*np.pi)/3)+1)*0.01
+y = np.linspace(0,500,100000)
+plt.plot(y,vaccf(y))
 plt.show()
+
 
 
