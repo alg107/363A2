1e010a6280e62583a3b5f418e43ebc650a7f4936
Alex Goodenbour <alg107@uclive.ac.nz>
2020-10-07 15:50:12 +1300

    added different vaccine models

--- SIRV.py
+++ SIRV.py
@@ -15,7 +15,7 @@ default_params = {
         "gamma": 0.1,
         "nu": (1/(30*4)),
         "InfDays": 10,
-        "R0": [0.5, 1.0, 2.0, 3.0, 4.0],
+        "R0": [1.8],
         "intervals": [0,50,120,200, 300, 400, 450, 500],
         "y0": [1.0,0.0,0.0,0.0],
         "import_packet": 6e-7 # 3 of 5,000,000
@@ -23,10 +23,12 @@ default_params = {
 
 
 class Model:
-    def __init__(self, modelfn, vacc_function, params):
+    def __init__(self, modelfn, vacc_function, params, name=""):
         self.modelfn = modelfn
         self.vacc_function = vacc_function
         self.params = params
+        self.name = name
+        self.gen_noisefn(500)
 
     def run(self, vacc=True):
         fn = self.modelfn
@@ -52,11 +54,25 @@ class Model:
                 y0_int[1]+=import_packet # Imported COVID
                 y0_int = y0_int/np.sum(y0_int) #Normalise
             plt.figure()
+            if self.name: plt.title(self.name)
             plt.plot(final_t, final_sol[:,0])
             plt.plot(final_t, final_sol[:,1])
             plt.plot(final_t, final_sol[:,2])
             plt.plot(final_t, final_sol[:,3])
             plt.legend(["S","I","R","V"])
+        self.print_model_stats(final_t, final_sol)
+
+    def print_model_stats(self, final_t, final_sol):
+        max_I_index = np.argmax(final_sol[:,1])
+        max_I = final_sol[:,1][max_I_index]
+        max_I_day = final_t[max_I_index]
+        final_S = final_sol[:,0][-1]
+        final_I = final_sol[:,1][-1]
+        final_R = final_sol[:,2][-1]
+        # Violated hospital capacity?
+        print("Final Recovered/Deceased:", final_R)
+        print("Max Infected:", max_I)
+        print("Day of Max Infection:", max_I_day)
 
     def runsir(self,y0, R, ti, tf, fn, vaccf):
         gamma = self.params["gamma"]
--- main.py
+++ main.py
@@ -6,9 +6,20 @@ plt.style.use('Solarize_Light2')
 from scipy import signal
 
 
-vaccf = lambda x: 0.5*(signal.square(x/(2*np.pi)/3)+1)*0.002
-m = SIRV.Model(SIRV.sirv, vaccf, SIRV.default_params)
-m.gen_noisefn(500) # Arg must be as long as needed
-plt.plot(m.x, m.spl(m.x))
-m.run()
+periodic_vaccf = lambda x: 0.5*(signal.square(x/(2*np.pi)/3)+1)*0.002
+const_vaccf = lambda x: 0.001
+no_vaccf = lambda x: 0
+
+m1 = SIRV.Model(SIRV.sirv, periodic_vaccf, SIRV.default_params, name="Periodic")
+print("\nPeriodic\n")
+m1.run()
+
+m2 = SIRV.Model(SIRV.sirv, const_vaccf, SIRV.default_params, name="Constant")
+print("\nConstant\n")
+m2.run()
+
+m3 = SIRV.Model(SIRV.sirv, no_vaccf, SIRV.default_params, name="None")
+print("\nNone\n")
+m3.run()
+
 plt.show()
